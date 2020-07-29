8121960ed9037b106a9df80a11a3d7273282ecce
Anis BEN SAID <abensaid@mit.edu>
2020-07-29 02:10:22 -0400

    Laying down code structure

---
+++ pipeline/src/Utilis/InputHandlers.py
@@ -0,0 +1,4 @@
+class InputHandler:
+    '''
+    Handles the inputs i.e the ratings and reviews files and the product catalogue files
+    '''
---
+++ pipeline/src/main.py
@@ -0,0 +1,4 @@
+from OutputBuilders.KPIBuilder import *
+
+if __name__ == '__main__':
+    test()
